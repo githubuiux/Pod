f95603ac54125f0dff49b301ce78584cd415c886
githubuiux <uiuxista@gmail.com>
2023-05-29 16:14:38 +0300

    Initial Commit

---
+++ Pod/PodApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodApp.swift
+//  Pod
+//
+//  Created by Levent on 29.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct PodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
